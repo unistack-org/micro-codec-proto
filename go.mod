6c7986505f1ad6b8070f32baf198880f1107bbda
Vasiliy Tolstov <v.tolstov@unistack.org>
2020-11-24 16:25:09 +0300

    update codec
    Signed-off-by: Vasiliy Tolstov <v.tolstov@unistack.org>

--- go.mod
+++ go.mod
@@ -4,7 +4,6 @@ go 1.15
 
 require (
 	github.com/golang/protobuf v1.4.3
-	github.com/oxtoacart/bpool v0.0.0-20190530202638-03653db5a59c
-	github.com/unistack-org/micro/v3 v3.0.0-gamma
+	github.com/unistack-org/micro/v3 v3.0.2-0.20201124121447-71d82e9d5b25
 	google.golang.org/protobuf v1.25.0
 )
--- proto.go
+++ proto.go
@@ -5,60 +5,102 @@ import (
 	"io"
 	"io/ioutil"
 
+	oldproto "github.com/golang/protobuf/proto"
 	"github.com/unistack-org/micro/v3/codec"
 	"google.golang.org/protobuf/proto"
 )
 
-type Codec struct {
-	Conn io.ReadWriteCloser
+type protoCodec struct{}
+
+func (c *protoCodec) Marshal(v interface{}) ([]byte, error) {
+	switch m := v.(type) {
+	case *codec.Frame:
+		return m.Data, nil
+	case proto.Message:
+		return proto.Marshal(m)
+	case oldproto.Message:
+		return oldproto.Marshal(m)
+	}
+	return nil, codec.ErrInvalidMessage
 }
 
-func (c *Codec) ReadHeader(m *codec.Message, t codec.MessageType) error {
+func (c *protoCodec) Unmarshal(d []byte, v interface{}) error {
+	if d == nil {
+		return nil
+	}
+	switch m := v.(type) {
+	case *codec.Frame:
+		m.Data = d
+	case proto.Message:
+		return proto.Unmarshal(d, m)
+	case oldproto.Message:
+		return oldproto.Unmarshal(d, m)
+	}
+	return codec.ErrInvalidMessage
+}
+
+func (c *protoCodec) ReadHeader(conn io.ReadWriter, m *codec.Message, t codec.MessageType) error {
 	return nil
 }
 
-func (c *Codec) ReadBody(b interface{}) error {
+func (c *protoCodec) ReadBody(conn io.ReadWriter, b interface{}) error {
 	if b == nil {
 		return nil
 	}
-	buf, err := ioutil.ReadAll(c.Conn)
-	if err != nil {
-		return err
+	switch m := b.(type) {
+	case *codec.Frame:
+		buf, err := ioutil.ReadAll(conn)
+		if err != nil {
+			return err
+		}
+		m.Data = buf
+		return nil
+	case oldproto.Message:
+		buf, err := ioutil.ReadAll(conn)
+		if err != nil {
+			return err
+		}
+		return oldproto.Unmarshal(buf, m)
+	case proto.Message:
+		buf, err := ioutil.ReadAll(conn)
+		if err != nil {
+			return err
+		}
+		return proto.Unmarshal(buf, m)
 	}
-	m, ok := b.(proto.Message)
-	if !ok {
-		return codec.ErrInvalidMessage
-	}
-	return proto.Unmarshal(buf, m)
+	return codec.ErrInvalidMessage
 }
 
-func (c *Codec) Write(m *codec.Message, b interface{}) error {
+func (c *protoCodec) Write(conn io.ReadWriter, m *codec.Message, b interface{}) error {
 	if b == nil {
-		// Nothing to write
 		return nil
 	}
-	p, ok := b.(proto.Message)
-	if !ok {
-		return codec.ErrInvalidMessage
-	}
-	buf, err := proto.Marshal(p)
-	if err != nil {
+	switch m := b.(type) {
+	case *codec.Frame:
+		_, err := conn.Write(m.Data)
+		return err
+	case oldproto.Message:
+		buf, err := oldproto.Marshal(m)
+		if err != nil {
+			return err
+		}
+		_, err = conn.Write(buf)
+		return err
+	case proto.Message:
+		buf, err := proto.Marshal(m)
+		if err != nil {
+			return err
+		}
+		_, err = conn.Write(buf)
 		return err
 	}
-	_, err = c.Conn.Write(buf)
-	return err
+	return codec.ErrInvalidMessage
 }
 
-func (c *Codec) Close() error {
-	return c.Conn.Close()
-}
-
-func (c *Codec) String() string {
+func (c *protoCodec) String() string {
 	return "proto"
 }
 
-func NewCodec(c io.ReadWriteCloser) codec.Codec {
-	return &Codec{
-		Conn: c,
-	}
+func NewCodec() codec.Codec {
+	return &protoCodec{}
 }
