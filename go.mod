7125b0c2c4d1ba7b4bb733997136f26aa495b97a
Vasiliy Tolstov <v.tolstov@unistack.org>
2021-09-22 02:22:22 +0300

    update for latest micro
    Signed-off-by: Vasiliy Tolstov <v.tolstov@unistack.org>

--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module github.com/unistack-org/micro-codec-proto/v3
 go 1.16
 
 require (
-	github.com/unistack-org/micro/v3 v3.7.1
+	github.com/unistack-org/micro/v3 v3.7.3
 	google.golang.org/protobuf v1.27.1
 )
--- proto.go
+++ proto.go
@@ -9,20 +9,27 @@ import (
 	"google.golang.org/protobuf/proto"
 )
 
-type protoCodec struct{}
+type protoCodec struct {
+	opts codec.Options
+}
 
 const (
 	flattenTag = "flatten"
 )
 
-func (c *protoCodec) Marshal(v interface{}) ([]byte, error) {
+func (c *protoCodec) Marshal(v interface{}, opts ...codec.Option) ([]byte, error) {
 	switch m := v.(type) {
 	case nil:
 		return nil, nil
 	case *codec.Frame:
 		return m.Data, nil
 	case proto.Message:
-		if nv, nerr := rutil.StructFieldByTag(v, codec.DefaultTagName, flattenTag); nerr == nil {
+		options := c.opts
+		for _, o := range opts {
+			o(&options)
+		}
+
+		if nv, nerr := rutil.StructFieldByTag(v, options.TagName, flattenTag); nerr == nil {
 			if nm, ok := nv.(proto.Message); ok {
 				m = nm
 			}
@@ -32,7 +39,7 @@ func (c *protoCodec) Marshal(v interface{}) ([]byte, error) {
 	return nil, codec.ErrInvalidMessage
 }
 
-func (c *protoCodec) Unmarshal(d []byte, v interface{}) error {
+func (c *protoCodec) Unmarshal(d []byte, v interface{}, opts ...codec.Option) error {
 	if len(d) == 0 {
 		return nil
 	}
@@ -42,7 +49,12 @@ func (c *protoCodec) Unmarshal(d []byte, v interface{}) error {
 	case *codec.Frame:
 		m.Data = d
 	case proto.Message:
-		if nv, nerr := rutil.StructFieldByTag(v, codec.DefaultTagName, flattenTag); nerr == nil {
+		options := c.opts
+		for _, o := range opts {
+			o(&options)
+		}
+
+		if nv, nerr := rutil.StructFieldByTag(v, options.TagName, flattenTag); nerr == nil {
 			if nm, ok := nv.(proto.Message); ok {
 				m = nm
 			}
@@ -89,6 +101,6 @@ func (c *protoCodec) String() string {
 	return "proto"
 }
 
-func NewCodec() codec.Codec {
-	return &protoCodec{}
+func NewCodec(opts ...codec.Option) codec.Codec {
+	return &protoCodec{opts: codec.NewOptions(opts...)}
 }
