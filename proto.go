016b3aa878feca3e3d76036a5a387f8fead4a837
Vasiliy Tolstov <v.tolstov@unistack.org>
2021-09-24 00:24:58 +0300

    fix codec.Frame in case of flatten struct tag
    Signed-off-by: Vasiliy Tolstov <v.tolstov@unistack.org>

--- proto.go
+++ proto.go
@@ -13,55 +13,61 @@ type protoCodec struct {
 	opts codec.Options
 }
 
+var _ codec.Codec = &protoCodec{}
+
 const (
 	flattenTag = "flatten"
 )
 
 func (c *protoCodec) Marshal(v interface{}, opts ...codec.Option) ([]byte, error) {
-	switch m := v.(type) {
-	case nil:
+	if v == nil {
 		return nil, nil
-	case *codec.Frame:
-		return m.Data, nil
-	case proto.Message:
-		options := c.opts
-		for _, o := range opts {
-			o(&options)
-		}
-
-		if nv, nerr := rutil.StructFieldByTag(v, options.TagName, flattenTag); nerr == nil {
-			if nm, ok := nv.(proto.Message); ok {
-				m = nm
-			}
-		}
-		return proto.Marshal(m)
 	}
-	return nil, codec.ErrInvalidMessage
+
+	options := c.opts
+	for _, o := range opts {
+		o(&options)
+	}
+
+	if nv, nerr := rutil.StructFieldByTag(v, options.TagName, flattenTag); nerr == nil {
+		v = nv
+	}
+
+	if m, ok := v.(*codec.Frame); ok {
+		return m.Data, nil
+	}
+
+	if _, ok := v.(proto.Message); !ok {
+		return nil, codec.ErrInvalidMessage
+	}
+
+	return proto.Marshal(v.(proto.Message))
 }
 
 func (c *protoCodec) Unmarshal(d []byte, v interface{}, opts ...codec.Option) error {
-	if len(d) == 0 {
+	if v == nil || len(d) == 0 {
 		return nil
 	}
-	switch m := v.(type) {
-	case nil:
-		return nil
-	case *codec.Frame:
-		m.Data = d
-	case proto.Message:
-		options := c.opts
-		for _, o := range opts {
-			o(&options)
-		}
 
-		if nv, nerr := rutil.StructFieldByTag(v, options.TagName, flattenTag); nerr == nil {
-			if nm, ok := nv.(proto.Message); ok {
-				m = nm
-			}
-		}
-		return proto.Unmarshal(d, m)
+	options := c.opts
+	for _, o := range opts {
+		o(&options)
 	}
-	return codec.ErrInvalidMessage
+
+	if nv, nerr := rutil.StructFieldByTag(v, options.TagName, flattenTag); nerr == nil {
+		v = nv
+	}
+
+	if m, ok := v.(*codec.Frame); ok {
+		m.Data = d
+		return nil
+	}
+
+	if _, ok := v.(proto.Message); !ok {
+		return codec.ErrInvalidMessage
+	}
+
+	return proto.Unmarshal(d, v.(proto.Message))
 }
 
 func (c *protoCodec) ReadHeader(conn io.Reader, m *codec.Message, t codec.MessageType) error {
